49d3fea7d503198be7a75c255a9481dbeeae347f
dranefor <savelamaksim011@gmail.com>
2026-03-11 13:43:32 +0200

    Implement Item2d class with Serializable interface

--- src/main/java/ex01/Item2d.java
+++ src/main/java/ex01/Item2d.java
@@ -1,4 +1,18 @@
 package ex01;
 
-public class Item2d {
+import java.io.Serializable;
+
+public class Item2d implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public double u;
+    public double r;
+
+
+    public transient String binaryResult;
+
+    public Item2d(double u, double r) {
+        this.u = u;
+        this.r = r;
+    }
 }

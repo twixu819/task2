e8bbf9ce74c50112472311d15fae35b3845a4c91
dranefor <savelamaksim011@gmail.com>
2026-03-11 13:44:25 +0200

    Implement main method for binary conversion test
    Added main method to Test class for testing binary conversion.

--- src/main/java/ex01/Test.java
+++ src/main/java/ex01/Test.java
@@ -1,4 +1,20 @@
 package ex01;
 
 public class Test {
+    public static void main(String[] args) {
+        View v = new View();
+        v.show();
+
+        System.out.println("\n test check");
+        double u = 100.0;
+        double r = 10.0;
+        int result = (int)(u / r);
+        String bin = Integer.toBinaryString(result);
+
+        if (bin.equals("1010")) {
+            System.out.println("test ok, 10 in binary is 1010");
+        } else {
+            System.out.println("test error");
+        }
+    }
 }

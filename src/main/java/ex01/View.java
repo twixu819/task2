68f0d2077d0c33ee7fd3c8e9aad10e512d574ff3
dranefor <savelamaksim011@gmail.com>
2026-03-11 13:45:14 +0200

    Add show method to View class for calculations
    Implement show method to display calculations and handle file operations

--- src/main/java/ex01/View.java
+++ src/main/java/ex01/View.java
@@ -1,4 +1,28 @@
 package ex01;
 
 public class View {
+    public void show() {
+        Calc c = new Calc();
+        double[] rs = {10.0, 20.0, 5.0, 2.0};
+
+        System.out.println("calculation");
+        c.calculate(100.0, rs);
+        printList(c);
+
+        try {
+            c.save();
+            System.out.println("\nsaved, being restored");
+            c.restore();
+            printList(c);
+        } catch (Exception e) {
+            System.out.println("error of file");
+        }
+    }
+
+    private void printList(Calc c) {
+        for (int i = 0; i < c.list.size(); i++) {
+            Item2d item = c.list.get(i);
+            System.out.println("U: " + item.u + " R: " + item.r + " I: " + item.binaryResult);
+        }
+    }
 }

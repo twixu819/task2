740d90ee875a93e093bd8da4bf23f9648ed49eb8
dranefor <savelamaksim011@gmail.com>
2026-03-11 13:42:59 +0200

    Add calculation, save, and restore methods in Calc
    Implement methods for calculation, saving, and restoring items.

--- src/main/java/ex01/Calc.java
+++ src/main/java/ex01/Calc.java
@@ -1,4 +1,32 @@
 package ex01;
 
+import java.util.ArrayList;
+import java.io.*;
+
+
 public class Calc {
+
+    public ArrayList<Item2d> list = new ArrayList<Item2d>();
+
+    public void calculate(double u, double[] rs) {
+        list.clear();
+        for (int i = 0; i < rs.length; i++) {
+            Item2d item = new Item2d(u, rs[i]);
+            int iValue = (int)(u / rs[i]);
+            item.binaryResult = Integer.toBinaryString(iValue);
+            list.add(item);
+        }
+    }
+
+    public void save() throws IOException {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("save.bin"));
+        out.writeObject(list);
+        out.close();
+    }
+
+    public void restore() throws Exception {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("save.bin"));
+        list = (ArrayList<Item2d>) in.readObject();
+        in.close();
+    }
 }
